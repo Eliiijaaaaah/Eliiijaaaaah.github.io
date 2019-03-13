e190a6becdf794c2c740a2982f3423f9e6ebdec4
Elijah T Davis <elijahit@purdue.edu>
2019-03-13 02:17:38 -0400

    Messing with routing

---
+++ resources/js/routing.js
@@ -0,0 +1,45 @@
+// getElementById wrapper
+function $id(id) {
+  return document.getElementById(id);
+}
+
+// asyncrhonously fetch the html template partial from the file directory,
+// then set its contents to the html of the parent element
+function loadHTML(url, id) {
+  req = new XMLHttpRequest();
+  req.open('GETJSON', url);
+  req.send();
+  req.onload = () => {
+    $id(id).innerHTML = req.responseText;
+  };
+}
+
+// use #! to hash
+router = new Navigo(null, true, '#');
+router.on({
+  // 'view' is the id of the div element inside which we render the HTML
+  'firstroute': () => { loadHTML('./pages/home.html', 'view'); },
+});
+
+// set the default route
+router.on(() => { $id('view').innerHTML = '<h2>Here by default</h2>'; });
+
+router.on('/about', function () {
+    // display about page
+		loadHTML('./pages/about.html', 'view');
+  }).resolve();
+
+router.on('home', function () {
+    // display about page
+		loadHTML('./pages/home.html', 'view');
+  }).resolve();
+
+// set the 404 route
+router.notFound((query) => { $id('view').innerHTML = '<h3>Couldn\'t find the page you\'re looking for...</h3>'; });
+
+router.resolve();
+
+function changePage(page){
+	console.log(page);
+	router.navigate(page);
+}
